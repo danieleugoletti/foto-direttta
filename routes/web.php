026ed72ef611b1a5bdffe7c911f1af527cc1d0ab
Daniele Ugoletti <daniele.ugoletti@gruppometa.it>
2020-03-29 10:51:35 +0200

    Removed registration and email verification

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
+Auth::routes(['register' => false, 'confirm' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
