90b7fea5a1d416cc9c3d3a82a73fd70d3272d7dc
Daniele Ugoletti <daniele.ugoletti@gruppometa.it>
2020-04-19 22:57:31 +0200

    Added social meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="{{ config('app.name', 'Laravel') }}">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="">
+    <meta property="og:url" content="">
+    <meta name="twitter:card" content="summary_large_image">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
