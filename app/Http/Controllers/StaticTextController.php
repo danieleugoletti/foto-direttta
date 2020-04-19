b1ab69737c66b5b3ed873e67d9edfc0573f39ac9
Daniele Ugoletti <daniele.ugoletti@gruppometa.it>
2020-04-19 10:17:50 +0200

    Convert MD text with heading anchor

--- app/Http/Controllers/StaticTextController.php
+++ app/Http/Controllers/StaticTextController.php
@@ -4,22 +4,49 @@ namespace App\Http\Controllers;
 
 use League\CommonMark\CommonMarkConverter;
 use League\CommonMark\Environment;
-use League\CommonMark\Extension\Table\TableExtension;
-
+use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension;
+use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkRenderer;
 
 class StaticTextController extends Controller
 {
-
     /**
      * @return \Illuminate\Http\Response
      */
     public function __invoke($url)
     {
-        abort_unless($page = @file_get_contents(resource_path('static-text').'/'.$url.'.md'), 404);
+        $text = $this->convertMDinHtml($this->readMDText($url));
 
-        $converter = new CommonMarkConverter();
         return view('static-text', [
-            'content' => $converter->convertToHtml($page),
+            'content' => $text,
         ]);
+    }
+
+    /**
+     * @param  string $pageName
+     * @return string
+     */
+    private function readMDText($pageName)
+    {
+        abort_unless($text = @file_get_contents(resource_path('static-text').'/'.$pageName.'.md'), 404);
+
+        return $text;
+    }
+
+    /**
+     * @param  string $text
+     * @return string
+     */
+    private function convertMDinHtml($text)
+    {
+        $environment = Environment::createCommonMarkEnvironment();
+        $environment->addExtension(new HeadingPermalinkExtension());
+        $config = [
+            'heading_permalink' => [
+                'inner_contents' => '',
+            ],
+        ];
+
+        $converter = new CommonMarkConverter($config, $environment);
+        return $converter->convertToHtml($text);
     }
 }
